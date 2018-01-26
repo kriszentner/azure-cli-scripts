5b1f0e1fac40e2ae2f2f2563b70830e5508a6a0c
Kris Zentner <kzentner@section6.net>
2018-01-26 09:56:47 -0800

    Create redeploy.sh

---
+++ redeploy.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+#
+# Author: Kris Zentner
+# Date January, 2018
+#
+# This is meant to reallocate a hung Azure Linux VM, similar to the "reallocate" 
+# portal command, but this seems to work better in most cases.
+# This uses jq as a dependency, so be sure to intsall it.
+# Not responsible for any damages as a result of using this script!
+# Test it yourself before using.
+#
+VMNAME="myvm"
+RG="my_rg"
+SUBSCRIPTION="my subscription"
+az account set --subscription "$SUBSCRIPTION"
+AVSET=$(az vm show -g $RG -n $VMNAME |jq '.availabilitySet.id'|awk -F/ '{ print $NF }'|sed 's/"//g')
+OSDISK=$(az vm show -g $RG -n $VMNAME |jq '.storageProfile.osDisk.managedDisk.id'|sed 's/"//g')
+DATADISKS=$(az vm show -g $RG -n $VMNAME |jq '.storageProfile.dataDisks'|jq '.[].name'|sed 's/"//g'|tr '\n' ' ')
+OSDISKNAME=$(echo $OSDISK|awk -F/ '{ print $NF }')
+NIC=$(az vm show -g $RG -n $VMNAME |jq '.networkProfile.networkInterfaces'|jq '.[0].id'|awk -F/ '{ print $NF }'|sed 's/"//g')
+LOCATION=$(az vm show -g $RG -n $VMNAME |jq '.location'|sed 's/"//g')
+SIZE=$(az vm show -g $RG -n $VMNAME |jq '.hardwareProfile.vmSize'|sed 's/"//g')
+echo "Removing old VM $VMNAME"
+az vm delete -g $RG -n $VMNAME --yes
+# If you want to attach a disk to another VM
+#
+# NEWVMNAME="my new vm"
+# az vm disk attach -g $RG --vm-name $NEWVMNAME --disk $OSDISK
+# and then detach
+# az vm disk detach -g $RG --vm-name $NEWVMNAME --name $OSDISKNAME
+#
+echo "Recreating VM $VMNAME"
+az vm create -g $RG -n $VMNAME \
+  --nics $NIC \
+  --attach-os-disk $OSDISK \
+  if [ "$AVSET" != "null" ];then
+  --availability-set $AVSET \
+  fi
+  if [ ! -z $DATADISKS ];then
+  --attach-data-disks $DATADISKS
+  fi
+  --size $SIZE \
+  --location $LOCATION \
+  --os-type Linux
