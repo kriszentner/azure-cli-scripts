1d427e8e7457d0526f87c03d2a2edca393eb590e
Kris Zentner <kzentner@section6.net>
2018-01-26 11:10:28 -0800

    typeo

--- redeploy.sh
+++ redeploy.sh
@@ -5,7 +5,7 @@
 #
 # This is meant to reallocate a hung Azure Linux VM, similar to the "reallocate" 
 # portal command, but this seems to work better in most cases.
-# This uses jq as a dependency, so be sure to intsall it.
+# This uses jq as a dependency, so be sure to install it.
 # Not responsible for any damages as a result of using this script!
 # Test it yourself before using.
 #
